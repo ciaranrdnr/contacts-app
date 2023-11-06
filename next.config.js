828a8685406037cf5fcda11cef0067e83221103a
Ciara <ciaranurdenara@gmail.com>
2023-11-07 00:23:33 +0700

    Update: bundle config

--- next.config.js
+++ next.config.js
@@ -7,8 +7,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-
-
 const nextConfig = {
   reactStrictMode: true,
    webpack: (config, { dev, isServer }) => {
