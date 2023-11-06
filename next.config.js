d5f03d940b292d4f694b9ea212a09adbc584497f
Ciara <ciaranurdenara@gmail.com>
2023-11-07 01:11:57 +0700

    Update: add pwa

--- next.config.js
+++ next.config.js
@@ -3,12 +3,34 @@ const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+const runtimeCaching = require('next-pwa/cache')  
+
+const isBuildPwa = (process.env.NODE_EN === 'development') || (process.env.DEBUG_PWA === 'true');
 
 const nextConfig = {
   reactStrictMode: true,
+   pwa: {
+    disable: !isBuildPwa,
+    register: isBuildPwa,
+    dest: 'public',
+    runtimeCaching,
+    publicExcludes: ['!static/*'],
+    buildExcludes: [
+      /\.(?:map|)$/i,
+      ({ asset }) => {
+        if (asset.name.match(/\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i)) {
+          return true;
+        }
+        if (asset.name.endsWith('.js')) {
+          return true;
+        }
+      },
+    ],
+    additionalManifestEntries: [],
+  },
    webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
       config.plugins.push(new CleanWebpackPlugin());
@@ -30,6 +52,6 @@ const nextConfig = {
   }
 }
 
-module.exports = withBundleAnalyzer({
+module.exports = withPWA({
   nextConfig
 });
